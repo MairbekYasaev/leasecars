42bdef1885520680876486415f3407b7c36806bc
Mairbek <mairbekyasaev007@gmail.com>
2023-04-06 16:42:42 +0300

    06.04.2023

--- js/main.js
+++ js/main.js
@@ -1,10 +1,43 @@
-const swiper = new Swiper(".swiper", {
-  effect: "fade",
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-  });
+
+  const tabItem = document.querySelectorAll('.tabs__btn-item');
+  const tabContent = document.querySelectorAll('.tabs__content-item');
+
+  tabItem.forEach(function(item){
+    item.addEventListener('click', open);
+  })
+
+  function open (evt){
+    const tabTarget = evt.currentTarget;
+    const button = tabTarget.dataset.button;
+
+    tabItem.forEach(function(item){
+      item.classList.remove('tabs__btn-item--active');
+    });
+
+    tabTarget.classList.add('tabs__btn-item--active');
+
+    tabItem.forEach(function(item){
+      item.classList.remove('tabs__btn-item--active');
+    })
+
+    tabTarget.classList.add('tabs__btn-item--active');
+
+    document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+  
+  }
+
+
+
+
+
+
+  const swiper = new Swiper(".swiper", {
+    effect: "fade",
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    });
